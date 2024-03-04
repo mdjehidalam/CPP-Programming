aceb0a97bc8612f1107d56ef5efa91938325cf1f
mdjehidalam <146694343+mdjehidalam@users.noreply.github.com>
2024-03-04 17:50:45 +0530

    LowerCaseAlphabetsConvertIntoUpperCaseAlphabets.cpp
    without using inbuid function

---
+++ LowerCaseAlphabetsConvertInotUpperCaseAlphabets.cpp
@@ -0,0 +1,19 @@
+//LowerCase convert into UpperCase charachar for example: a into A and b into B
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main(){
+    string s;//create an string 
+    cin>>s; //taking input from user
+    int i=0;
+    while(s[i]!='\0')// repeating until nullcharachar('\0')
+    {
+        if(s[i]>= 'a' && s[i] <='z')
+        {
+            s[i]=s[i]-32;//here is converting lowerCase into UpperCase
+        }
+        i++;
+    }
+     cout<<s<<endl;//Printing string 
+ }
+ 
+ 
