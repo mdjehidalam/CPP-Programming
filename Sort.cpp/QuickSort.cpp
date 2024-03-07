74ba63a7333c6ae0ecced7ecee177c85c8314f44
mdjehidalam <146694343+mdjehidalam@users.noreply.github.com>
2024-03-07 13:07:43 +0530

    Add files via upload

---
+++ Sort.cpp/QuickSort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int partition(int a[],int s,int e){
+     int i=s;
+     int pivot = a[e];
+     for(int j=s;j<=e-1;j++){
+        if(a[j] < pivot){
+            swap(a[i],a[j]);
+            i++;
+        }
+     }
+     swap(a[i],a[e]);
+     return i;
+}
+
+void quickSort(int a[],int s,int e){
+    if(s>=e){
+        return;
+    }
+    int p = partition(a,s,e);
+    quickSort(a,s,p-1);
+    quickSort(a,p+1,e);
+}
+
+int main(){
+    int a[]={3,4,6,1,8};
+    quickSort(a,0,5);
+    for(int i=0; i<5; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+   return 0;
+}
