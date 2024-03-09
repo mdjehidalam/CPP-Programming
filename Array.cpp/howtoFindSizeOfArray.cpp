83e10c86f7ace10faeb80acf6ef533aabbd6990a
mdjehidalam <146694343+mdjehidalam@users.noreply.github.com>
2024-03-09 11:43:34 +0530

    Create howtoFindSizeOfArray.cpp

---
+++ Array.cpp/howtoFindSizeOfArray.cpp
@@ -0,0 +1,10 @@
+ 
+#include <iostream>
+using namespace std;
+int main() {
+      int a[]={1, 4, 3, 1, 3, 5};
+      int size=sizeof(a)/sizeof(0);// obtain size of array
+      cout<<"Size: "<<size<<endl;// print size of array 
+
+    return 0;
+}
